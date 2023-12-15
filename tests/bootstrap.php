d9d9f15c2048e04944f23d0338eaf325a2333bee
Anupam Kumar <kyteinsky@gmail.com>
2023-12-15 20:11:47 +0530

    update phpunit config and composer
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-\OC_App::loadApp(OCA\ContextChat\AppInfo\Application::APP_ID);
+use \OCA\ContextChat\AppInfo\Application;
+use \OCP\App\IAppManager;
+
+\OC::$server->get(IAppManager::class)->loadApp(Application::APP_ID);
 OC_Hook::clear();
