fa89bc9d7ea08fd1cfd2e2faafb0d90000fff3bf
Anupam Kumar <kyteinsky@gmail.com>
2023-12-15 19:07:38 +0530

    add multiple mimetypes support
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -36,6 +36,22 @@ class Application extends App implements IBootstrap {
 		'text/plain',
 		'text/markdown',
 		'application/json',
+		'application/pdf',
+		'text/csv',
+		'application/epub+zip',
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+		'application/vnd.ms-powerpoint',
+		'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		'application/vnd.oasis.opendocument.spreadsheet',
+		'application/vnd.ms-excel.sheet.macroEnabled.12',
+		'application/vnd.oasis.opendocument.text',
+		'text/rtf',
+		'text/x-rst',
+		'application/xml',
+		'message/rfc822',
+		'application/vnd.ms-outlook',
+		'text/org',
 	];
 
 	public function __construct(array $urlParams = []) {
