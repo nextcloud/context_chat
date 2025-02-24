57173675377c2d630224fa5ee82e52fd7f7daab7
Marcel Klehr <mklehr@gmx.net>
2025-02-24 12:14:49 +0100

    fix: Add AppGlobal mixin
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './components/ViewAdmin.vue'
+import AppGlobal from './mixins/AppGlobal.js'
+
+Vue.mixin(AppGlobal)
 
 global.ContextChat = new Vue({
 	el: '#context_chat',
