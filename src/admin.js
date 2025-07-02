df7170f5c28e31fcfbba9128f33be9cd97463d8d
Lukas Schaefer <lukas@lschaefer.xyz>
2025-07-02 10:33:42 -0400

    chore(deps): upgrade to vue 3 and vite
    Signed-off-by: Lukas Schaefer <lukas@lschaefer.xyz>

--- src/admin.js
+++ src/admin.js
@@ -2,13 +2,11 @@
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './components/ViewAdmin.vue'
 import AppGlobal from './mixins/AppGlobal.js'
 
-Vue.mixin(AppGlobal)
+const app = createApp(App)
+app.mixin(AppGlobal)
 
-global.ContextChat = new Vue({
-	el: '#context_chat',
-	render: h => h(App),
-})
+app.mount('#context_chat')
