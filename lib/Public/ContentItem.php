174c9f022847cbb03b6c4c7e24884337a097005f
Edward Ly <contact@edward.ly>
2025-07-08 09:39:25 -0700

    refactor: extend OCP API for public classes
    Signed-off-by: Edward Ly <contact@edward.ly>

--- lib/Public/ContentItem.php
+++ lib/Public/ContentItem.php
@@ -7,24 +7,5 @@
 
 namespace OCA\ContextChat\Public;
 
-class ContentItem {
-	/**
-	 * @param string $itemId
-	 * @param string $providerId
-	 * @param string $title
-	 * @param string $content
-	 * @param string $documentType
-	 * @param \DateTime $lastModified
-	 * @param string[] $users
-	 */
-	public function __construct(
-		public string $itemId,
-		public string $providerId,
-		public string $title,
-		public string $content,
-		public string $documentType,
-		public \DateTime $lastModified,
-		public array $users,
-	) {
-	}
+class ContentItem extends \OCP\ContextChat\ContentItem {
 }
