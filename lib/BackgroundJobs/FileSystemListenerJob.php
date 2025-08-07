861b9eae08486577bd0a6e7f850e34be67c72ba2
Marcel Klehr <mklehr@gmx.net>
2025-08-07 12:44:22 +0200

    fix(FileSystemListenerJob): Prevent OOM
    by working per user and tearing down FS in between
    
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/BackgroundJobs/FileSystemListenerJob.php
+++ lib/BackgroundJobs/FileSystemListenerJob.php
@@ -63,43 +63,55 @@ class FileSystemListenerJob extends TimedJob {
 				return;
 			}
 
+			$eventsByUser = [];
 			foreach ($fsEvents as $fsEvent) {
-				$this->diagnosticService->sendHeartbeat(static::class, $this->getId());
-
-				try {
-					$node = current($this->rootFolder->getUserFolder($fsEvent->getUserId())->getById($fsEvent->getNodeId()));
-				} catch (\Exception $e) {
-					$this->logger->warning('Error retrieving node for fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
-					$node = false;
+				if (!isset($eventsByUser[$fsEvent->getUserId()])) {
+					$eventsByUser[$fsEvent->getUserId()] = [];
 				}
-				if ($node === false) {
-					$this->logger->warning('Node with ID ' . $fsEvent->getNodeId() . ' not found for fs event "' . $fsEvent->getType() . '"');
+				$eventsByUser[$fsEvent->getUserId()][] = $fsEvent;
+			}
+
+			foreach ($eventsByUser as $userId => $events) {
+				foreach ($events as $fsEvent) {
+					$this->diagnosticService->sendHeartbeat(static::class, $this->getId());
+
+					try {
+						$node = current($this->rootFolder->getUserFolder($fsEvent->getUserId())->getById($fsEvent->getNodeId()));
+					} catch (\Exception $e) {
+						$this->logger->warning('Error retrieving node for fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
+						$node = false;
+					}
+					if ($node === false) {
+						$this->logger->warning('Node with ID ' . $fsEvent->getNodeId() . ' not found for fs event "' . $fsEvent->getType() . '"');
+						try {
+							$this->fsEventMapper->delete($fsEvent);
+						} catch (Exception $e) {
+							$this->logger->warning('Error deleting fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
+						}
+						continue;
+					}
+
+					try {
+						switch ($fsEvent->getTypeObject()) {
+							case FsEventType::CREATE:
+								$this->fsEventService->onInsert($node);
+								break;
+							case FsEventType::ACCESS_UPDATE_DECL:
+								$this->fsEventService->onAccessUpdateDecl($node);
+								break;
+						}
+						$this->diagnosticService->sendHeartbeat(static::class, $this->getId());
+					} catch (\Throwable $e) {
+						$this->logger->warning('Error handling fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
+					}
 					try {
 						$this->fsEventMapper->delete($fsEvent);
 					} catch (Exception $e) {
 						$this->logger->warning('Error deleting fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
 					}
-					continue;
 				}
-
-				try {
-					switch ($fsEvent->getTypeObject()) {
-						case FsEventType::CREATE:
-							$this->fsEventService->onInsert($node);
-							break;
-						case FsEventType::ACCESS_UPDATE_DECL:
-							$this->fsEventService->onAccessUpdateDecl($node);
-							break;
-					}
-					$this->diagnosticService->sendHeartbeat(static::class, $this->getId());
-				} catch (\Throwable $e) {
-					$this->logger->warning('Error handling fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
-				}
-				try {
-					$this->fsEventMapper->delete($fsEvent);
-				} catch (Exception $e) {
-					$this->logger->warning('Error deleting fs event "' . $fsEvent->getType() . '": ' . $e->getMessage(), ['exception' => $e]);
-				}
+				// Tear down to avoid memory leaks
+				\OC_Util::tearDownFS();
 			}
 		} finally {
 			$this->diagnosticService->sendJobEnd(static::class, $this->getId());
