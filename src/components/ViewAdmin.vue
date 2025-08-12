ace3b81c500a30b3b142a388d84770d2b9cbd54d
Lukas Schaefer <lukas@lschaefer.xyz>
2025-08-12 08:32:36 -0400

    change to h3
    Signed-off-by: Lukas Schaefer <lukas@lschaefer.xyz>

--- src/components/ViewAdmin.vue
+++ src/components/ViewAdmin.vue
@@ -6,74 +6,72 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <template>
 	<div id="context_chat">
 		<NcSettingsSection :name="t('context_chat', 'Context Chat')">
-			<NcSettingsSection :name="t('context_chat', 'Indexing Status')">
-				<NcNoteCard v-if="stats.initial_indexing_complete" show-alert type="success">
-					{{
-						t('context_chat', 'The initial indexing run finished at: {date}.', {date: showDate(stats.intial_indexing_completed_at)})
-					}}
-				</NcNoteCard>
-				<NcNoteCard v-else type="warning">
-					{{ t('context_chat', 'The initial indexing is still running.') }}
-				</NcNoteCard>
-				<NcNoteCard v-if="stats.backend_available" show-alert type="success">
-					{{ t('context_chat', 'The Context Chat Backend app is installed and responsive.') }}
-				</NcNoteCard>
-				<NcNoteCard v-else type="warning">
-					{{ t('context_chat', 'The Context Chat Backend app is not installed or not responsing.') }}
-				</NcNoteCard>
-				<NcNoteCard
-					v-if="stats.initial_indexing_complete && stats.eligible_files_count > stats.vectordb_document_counts['files__default'] * 1.2"
-					type="warning">
-					{{
-						t('context_chat', 'Less files were indexed than expected. Only {percent}% files out of {eligibleCount} are in the VectorDB.', {
-							percent: Math.round((stats.vectordb_document_counts['files__default'] / stats.eligible_files_count) * 100),
-							eligibleCount: stats.eligible_files_count
-						})
-					}}
-				</NcNoteCard>
-				<table>
-					<thead>
-						<tr>
-							<th>{{ t('context_chat', 'Content provider') }}</th>
-							<th>{{ t('context_chat', 'Queued documents') }}</th>
-							<th>{{ t('context_chat', 'Documents in vector database') }}</th>
-						</tr>
-					</thead>
-					<tr v-for="(count, providerId) in stats.queued_documents_counts" :key="providerId">
-						<td>{{ providerId }}</td>
-						<td>{{ count }}</td>
-						<td v-if="stats.vectordb_document_counts">
-							{{ stats.vectordb_document_counts[providerId] }}
-							<template v-if="providerId === 'files__default'">
-								{{ t('context_chat', '(out of {count} sent)', {count: stats.indexed_files_count}) }}
-							</template>
-						</td>
-						<td v-else>
-							{{ t('context_chat', 'Not available') }}
-						</td>
+			<h3>{{ t('context_chat', 'Indexing Status') }}</h3>
+			<NcNoteCard v-if="stats.initial_indexing_complete" show-alert type="success">
+				{{
+					t('context_chat', 'The initial indexing run finished at: {date}.', {date: showDate(stats.intial_indexing_completed_at)})
+				}}
+			</NcNoteCard>
+			<NcNoteCard v-else type="warning">
+				{{ t('context_chat', 'The initial indexing is still running.') }}
+			</NcNoteCard>
+			<NcNoteCard v-if="stats.backend_available" show-alert type="success">
+				{{ t('context_chat', 'The Context Chat Backend app is installed and responsive.') }}
+			</NcNoteCard>
+			<NcNoteCard v-else type="warning">
+				{{ t('context_chat', 'The Context Chat Backend app is not installed or not responsing.') }}
+			</NcNoteCard>
+			<NcNoteCard
+				v-if="stats.initial_indexing_complete && stats.eligible_files_count > stats.vectordb_document_counts['files__default'] * 1.2"
+				type="warning">
+				{{
+					t('context_chat', 'Less files were indexed than expected. Only {percent}% files out of {eligibleCount} are in the VectorDB.', {
+						percent: Math.round((stats.vectordb_document_counts['files__default'] / stats.eligible_files_count) * 100),
+						eligibleCount: stats.eligible_files_count
+					})
+				}}
+			</NcNoteCard>
+			<table>
+				<thead>
+					<tr>
+						<th>{{ t('context_chat', 'Content provider') }}</th>
+						<th>{{ t('context_chat', 'Queued documents') }}</th>
+						<th>{{ t('context_chat', 'Documents in vector database') }}</th>
 					</tr>
-				</table>
-				<p>&nbsp;</p>
-				<p>{{ t('context_chat', 'Eligible files for indexing: {count}', {count: stats.eligible_files_count}) }}</p>
-				<p>{{ t('context_chat', 'Queued content update actions: {count}', {count: stats.queued_actions_count}) }}</p>
-				<p>{{ t('context_chat', 'Queued File System events: {count}', {count: stats.queued_fs_events_count}) }}</p>
-			</NcSettingsSection>
-			<NcSettingsSection :name="t('context_chat', 'Download Logs')">
-				<div class="horizontal-flex">
-					<NcButton :href="downloadURLNextcloudLogs">
-						{{ t('context_chat', 'Download the PHP App logs') }}
-					</NcButton>
-					<NcButton :href="downloadURLDockerLogs">
-						{{ t('context_chat', 'Download the Ex-App Backend logs') }}
-					</NcButton>
-				</div>
-				<p>&nbsp;</p>
-				<p>
-					<a href="https://docs.nextcloud.com/server/latest/admin_manual/ai/app_context_chat.html">{{
-						t('context_chat', 'Official documentation')
-					}}</a>
-				</p>
-			</NcSettingsSection>
+				</thead>
+				<tr v-for="(count, providerId) in stats.queued_documents_counts" :key="providerId">
+					<td>{{ providerId }}</td>
+					<td>{{ count }}</td>
+					<td v-if="stats.vectordb_document_counts">
+						{{ stats.vectordb_document_counts[providerId] }}
+						<template v-if="providerId === 'files__default'">
+							{{ t('context_chat', '(out of {count} sent)', {count: stats.indexed_files_count}) }}
+						</template>
+					</td>
+					<td v-else>
+						{{ t('context_chat', 'Not available') }}
+					</td>
+				</tr>
+			</table>
+			<p>&nbsp;</p>
+			<p>{{ t('context_chat', 'Eligible files for indexing: {count}', {count: stats.eligible_files_count}) }}</p>
+			<p>{{ t('context_chat', 'Queued content update actions: {count}', {count: stats.queued_actions_count}) }}</p>
+			<p>{{ t('context_chat', 'Queued File System events: {count}', {count: stats.queued_fs_events_count}) }}</p>
+			<h3>{{ t('context_chat', 'Download Logs') }}</h3>
+			<div class="horizontal-flex">
+				<NcButton :href="downloadURLNextcloudLogs">
+					{{ t('context_chat', 'Download the PHP App logs') }}
+				</NcButton>
+				<NcButton :href="downloadURLDockerLogs">
+					{{ t('context_chat', 'Download the Ex-App Backend logs') }}
+				</NcButton>
+			</div>
+			<p>&nbsp;</p>
+			<p>
+				<a href="https://docs.nextcloud.com/server/latest/admin_manual/ai/app_context_chat.html">{{
+					t('context_chat', 'Official documentation')
+				}}</a>
+			</p>
 		</NcSettingsSection>
 	</div>
 </template>
