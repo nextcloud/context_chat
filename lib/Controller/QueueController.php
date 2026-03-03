68842a8660f54a73e6c8259fec33e268e51e1933
Marcel Klehr <mklehr@gmx.net>
2026-03-03 09:56:12 +0100

    chore: Add license for QueueController
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- lib/Controller/QueueController.php
+++ lib/Controller/QueueController.php
@@ -1,5 +1,13 @@
 <?php
 
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 namespace OCA\ContextChat\Controller;
 
 use OCA\ContextChat\Db\QueueActionMapper;
