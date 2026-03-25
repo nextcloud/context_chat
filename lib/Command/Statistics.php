642b3024f5593698473fa42c99b6fa4fa91174c5
Anupam Kumar <kyteinsky@gmail.com>
2026-03-25 12:06:18 +0530

    feat: add json output support to stats command
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- lib/Command/Statistics.php
+++ lib/Command/Statistics.php
@@ -9,74 +9,63 @@ declare(strict_types=1);
 
 namespace OCA\ContextChat\Command;
 
-use OCA\ContextChat\Db\FsEventMapper;
-use OCA\ContextChat\Db\QueueContentItemMapper;
-use OCA\ContextChat\Service\ActionScheduler;
-use OCA\ContextChat\Service\LangRopeService;
-use OCA\ContextChat\Service\QueueService;
-use OCA\ContextChat\Service\StorageService;
-use OCP\AppFramework\Services\IAppConfig;
-use OCP\Util;
+use OCA\ContextChat\Service\ProviderConfigService;
+use OCA\ContextChat\Service\StatisticsService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Statistics extends Command {
 
 	public function __construct(
-		private IAppConfig $appConfig,
-		private ActionScheduler $actionService,
-		private QueueService $queueService,
-		private StorageService $storageService,
-		private LangRopeService $langRopeService,
-		private QueueContentItemMapper $contentQueue,
-		private FsEventMapper $fsEventMapper,
+		private StatisticsService $statisticsService,
 	) {
 		parent::__construct();
 	}
 
 	protected function configure() {
 		$this->setName('context_chat:stats')
-			->setDescription('Check ContextChat statistics');
+			->setDescription('Check ContextChat statistics')
+			->addOption(
+				'json',
+				null,
+				InputOption::VALUE_NONE,
+				'Output the statistics in json format',
+			);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$stats = $this->statisticsService->getStatistics();
+		if ($input->getOption('json')) {
+			$prettyJson = json_encode($stats, JSON_PRETTY_PRINT);
+			if ($prettyJson === false) {
+				$output->writeln('{"error": "Failed to encode statistics to JSON."}');
+				return 1;
+			}
+			$output->writeln($prettyJson);
+			return 0;
+		}
+
 		$output->writeln('ContextChat statistics:');
-		if ($this->appConfig->getAppValueInt('last_indexed_time', 0, lazy: true) === 0) {
+		$output->writeln('Installed time: ' . (new \DateTime('@' . $stats['installed_at']))->format('Y-m-d H:i') . ' UTC');
+		if (!$stats['initial_indexing_complete']) {
 			$output->writeln('The indexing is not complete yet.');
 		} else {
-			$installedTime = $this->appConfig->getAppValueInt('installed_time', 0, lazy: true);
-			$lastIndexedTime = $this->appConfig->getAppValueInt('last_indexed_time', 0, lazy: true);
-			$indexTime = $lastIndexedTime - $installedTime;
-
-			$output->writeln('Installed time: ' . (new \DateTime('@' . $installedTime))->format('Y-m-d H:i') . ' UTC');
-			$output->writeln('Index complete time: ' . (new \DateTime('@' . $lastIndexedTime))->format('Y-m-d H:i') . ' UTC');
+			$indexTime = $stats['intial_indexing_completed_at'] - $stats['installed_at'];
+			$output->writeln('Index complete time: ' . (new \DateTime('@' . ($stats['intial_indexing_completed_at'] ?? '')))->format('Y-m-d H:i') . ' UTC');
 			$output->writeln('Total time taken for complete index: ' . strval(floor($indexTime / (60 * 60 * 24))) . ' days ' . gmdate('H:i', $indexTime) . ' (hh:mm)');
 		}
 
-		$eligibleFilesCount = $this->storageService->countFiles();
-		$output->writeln('Total eligible files: ' . $eligibleFilesCount);
-
-		$queueCount = $this->queueService->count();
-		$output->writeln('Files in indexing queue: ' . $queueCount);
-
-		$queueNewCount = $this->queueService->countNewFiles();
-		$output->writeln('New files in indexing queue (without updates): ' . $queueNewCount);
-
-		$queuedDocumentsCount = $this->contentQueue->count();
-		$output->writeln('Queued documents (without files):' . var_export($queuedDocumentsCount, true));
-
-		$indexFilesCount = Util::numericToNumber(intval($this->appConfig->getAppValueString('indexed_files_count', '0', lazy: true)));
-		$output->writeln('Files successfully sent to backend: ' . strval($indexFilesCount));
-
-		$indexedDocumentsCount = $this->langRopeService->getIndexedDocumentsCounts();
-		$output->writeln('Indexed documents: ' . var_export($indexedDocumentsCount, true));
-
-		$actionsCount = $this->actionService->count();
-		$output->writeln('Actions in queue: ' . $actionsCount);
-
-		$fsEventsCount = $this->fsEventMapper->count();
-		$output->writeln('File system events in queue: ' . $fsEventsCount);
+		$output->writeln('Context Chat Backend reachable: ' . ($stats['backend_available'] ? 'Yes' : 'No'));
+		$output->writeln('Total eligible files: ' . $stats['eligible_files_count']);
+		$output->writeln('Files in indexing queue: ' . ($stats['queued_documents_counts'][ProviderConfigService::getDefaultProviderKey()] ?? 0));
+		$output->writeln('New files in indexing queue (without updates): ' . $stats['queued_new_files_count']);
+		$output->writeln('Queued documents:' . var_export($stats['queued_documents_counts'] ?? [], true));
+		$output->writeln('Files successfully sent to backend: ' . strval($stats['recorded_indexed_files_count']));
+		$output->writeln('Indexed documents: ' . var_export($stats['vectordb_document_counts'], true));
+		$output->writeln('Actions in queue: ' . $stats['queued_actions_count']);
+		$output->writeln('File system events in queue: ' . $stats['queued_fs_events_count']);
 		return 0;
 	}
 }
---
+++ lib/Service/StatisticsService.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\ContextChat\Service;
+
+use OCA\ContextChat\Db\FsEventMapper;
+use OCA\ContextChat\Db\QueueContentItemMapper;
+use OCA\ContextChat\Logger;
+use OCA\ContextChat\Service\ActionScheduler;
+use OCA\ContextChat\Service\LangRopeService;
+use OCA\ContextChat\Service\ProviderConfigService;
+use OCA\ContextChat\Service\QueueService;
+use OCA\ContextChat\Service\StorageService;
+use OCP\AppFramework\Services\IAppConfig;
+use OCP\DB\Exception;
+use OCP\Util;
+
+class StatisticsService {
+	public function __construct(
+		private IAppConfig $appConfig,
+		private ActionScheduler $actionService,
+		private QueueService $queueService,
+		private StorageService $storageService,
+		private LangRopeService $langRopeService,
+		private QueueContentItemMapper $contentQueue,
+		private Logger $logger,
+		private FsEventMapper $fsEventMapper,
+	) {
+	}
+
+	public function getStatistics(): array {
+		$stats = [];
+
+		$stats['installed_at'] = $this->appConfig->getAppValueInt('installed_time', 0, lazy: true);
+		if ($this->appConfig->getAppValueInt('last_indexed_time', 0, lazy: true) === 0) {
+			$stats['initial_indexing_complete'] = false;
+		} else {
+			$stats['initial_indexing_complete'] = true;
+			$stats['intial_indexing_completed_at'] = $this->appConfig->getAppValueInt('last_indexed_time', 0, lazy: true);
+		}
+
+		try {
+			$stats['eligible_files_count'] = $this->storageService->countFiles();
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['eligible_files_count'] = 0;
+		}
+		$stats['recorded_indexed_files_count'] = Util::numericToNumber(intval(
+			$this->appConfig->getAppValueString('indexed_files_count', '0', lazy: true)
+		));
+		try {
+			$stats['queued_actions_count'] = $this->actionService->count();
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['queued_actions_count'] = 0;
+		}
+		try {
+			$stats['queued_fs_events_count'] = $this->fsEventMapper->count();
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['queued_fs_events_count'] = 0;
+		}
+		try {
+			$stats['vectordb_document_counts'] = $this->langRopeService->getIndexedDocumentsCounts();
+			$stats['backend_available'] = true;
+		} catch (\RuntimeException $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['backend_available'] = false;
+			$stats['vectordb_document_counts'] = [ ProviderConfigService::getDefaultProviderKey() => 0 ];
+		}
+		try {
+			$queued_files_count = $this->queueService->count();
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$queued_files_count = 0;
+		}
+		try {
+			$stats['queued_new_files_count'] = $this->queueService->countNewFiles();
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['queued_new_files_count'] = 0;
+		}
+		try {
+			$stats['queued_documents_counts'] = $this->contentQueue->count();
+			$stats['queued_documents_counts'][ProviderConfigService::getDefaultProviderKey()] = $queued_files_count;
+		} catch (Exception $e) {
+			$this->logger->error($e->getMessage(), ['exception' => $e]);
+			$stats['queued_documents_counts'] = [];
+		}
+
+		return $stats;
+	}
+}
